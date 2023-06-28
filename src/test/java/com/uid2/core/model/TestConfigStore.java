1435adb7c1bf530203e5a4dcd8fa4a938d5c8487
Thomas Manson <thomas.manson@thetradedesk.com>
2023-06-28 15:47:17 +1000

    Refactored tests

--- src/test/java/com/uid2/core/model/TestConfigStore.java
+++ src/test/java/com/uid2/core/model/TestConfigStore.java
@@ -3,6 +3,7 @@ package com.uid2.core.model;
 import com.uid2.core.vertx.TestSiteSpecificMetadataPathDisabled;
 import io.vertx.core.json.Json;
 import io.vertx.core.json.JsonObject;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.IOException;
@@ -11,37 +12,37 @@ import static com.uid2.shared.Utils.readToEndAsString;
 import static org.junit.jupiter.api.Assertions.*;
 
 public class TestConfigStore {
-    @Test
-    void loadIntegerSuccess() throws IOException {
-        ConfigStore store = new ConfigStore();
+    private ConfigStore store;
+
+    @BeforeEach
+    void loadConfig() throws IOException {
+        store = new ConfigStore();
         store.load(((JsonObject) Json.decodeValue(openFile("/com.uid2.core/model/test-config.json"))));
+    }
+
+    @Test
+    void loadIntegerSuccess() {
         Integer token_lifespan = store.getInteger("att_token_lifetime_seconds");
 
         assertEquals(120, token_lifespan);
     }
 
     @Test
-    void loadMissingIntegerReturnsNull() throws IOException {
-        ConfigStore store = new ConfigStore();
-        store.load(((JsonObject) Json.decodeValue(openFile("/com.uid2.core/model/test-config.json"))));
+    void loadMissingIntegerReturnsNull() {
         Integer t = store.getInteger("missing_key");
 
         assertNull(t);
     }
 
     @Test
-    void loadIntegerDefaultForMissingKeySuccess() throws IOException {
-        ConfigStore store = new ConfigStore();
-        store.load(((JsonObject) Json.decodeValue(openFile("/com.uid2.core/model/test-config.json"))));
+    void loadIntegerDefaultForMissingKeySuccess() {
         Integer token_lifespan = store.getIntegerOrDefault("missing_key", 45);
 
         assertEquals(45, token_lifespan);
     }
 
     @Test
-    void loadIntegerDefaultForKnownKeySuccess() throws IOException {
-        ConfigStore store = new ConfigStore();
-        store.load(((JsonObject) Json.decodeValue(openFile("/com.uid2.core/model/test-config.json"))));
+    void loadIntegerDefaultForKnownKeySuccess() {
         Integer token_lifespan = store.getIntegerOrDefault("att_token_lifetime_seconds", 120);
 
         assertEquals(120, token_lifespan);
