c961749aee75f366c55b939c691081acb08bca1a
Thomas Manson <thomas.manson@thetradedesk.com>
2023-08-23 12:59:50 +1000

    Updated log message

--- src/main/java/com/uid2/core/service/OperatorJWTTokenProvider.java
+++ src/main/java/com/uid2/core/service/OperatorJWTTokenProvider.java
@@ -81,7 +81,7 @@ public class OperatorJWTTokenProvider {
         claims.put("enclaveType", enclaveType);
         claims.put("operatorVersion", operatorVersion);
 
-        LOGGER.debug(String.format("Creating token with: Issuer: %s, Audience: %s, Role: %s, SiteId: %s, EnclaveId: %s, EnclaveType: %s, OperatorVersion: %s", audience, issuer, roleString, siteId, enclaveId, enclaveType, operatorVersion));
+        LOGGER.debug(String.format("Creating token with: Issuer: %s, Audience: %s, Roles: %s, SiteId: %s, EnclaveId: %s, EnclaveType: %s, OperatorVersion: %s", audience, issuer, roleString, siteId, enclaveId, enclaveType, operatorVersion));
         return this.jwtTokenProvider.getJWT(expiresAt, this.clock.instant(), claims);
     }
 }
