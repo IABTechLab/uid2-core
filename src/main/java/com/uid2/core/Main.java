df660cb2245faebccc9d3364cfc89e7d14d6c710
Katherine Chen <katherine.chen@thetradedesk.com>
2025-02-05 10:20:57 +1100

    Use 8.0.34-alpha-191-SNAPSHOT shared

--- src/main/java/com/uid2/core/Main.java
+++ src/main/java/com/uid2/core/Main.java
@@ -149,8 +149,8 @@ public class Main {
                 }
 
                 var maaServerBaseUrl = ConfigStore.Global.getOrDefault(com.uid2.core.Const.Config.MaaServerBaseUrlProp, "https://sharedeus.eus.attest.azure.net");
-                attestationService.with("azure-cc", new AzureCCCoreAttestationService(maaServerBaseUrl, ConfigStore.Global.get(Const.Config.CorePublicUrlProp)));
-                attestationService.with("azure-cc-aks", new AzureCCAksCoreAttestationService(maaServerBaseUrl, ConfigStore.Global.get(Const.Config.CorePublicUrlProp)));
+                attestationService.with("azure-cc", new AzureCCCoreAttestationService(maaServerBaseUrl, ConfigStore.Global.get(Const.Config.CorePublicUrlProp), "azure-cc"));
+                attestationService.with("azure-cc-aks", new AzureCCCoreAttestationService(maaServerBaseUrl, ConfigStore.Global.get(Const.Config.CorePublicUrlProp), "azure-cc-aks"));
 
                 attestationService.with("gcp-oidc", new GcpOidcCoreAttestationService(corePublicUrl));
 
