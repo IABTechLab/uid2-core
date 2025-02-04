c017146b3b412ddb8dd8a21288e34d81992c9f65
Katherine Chen <katherine.chen@thetradedesk.com>
2025-02-04 14:37:22 +1100

    Use shared snapshot version to get AksAttestationService

--- src/main/java/com/uid2/core/Main.java
+++ src/main/java/com/uid2/core/Main.java
@@ -150,6 +150,7 @@ public class Main {
 
                 var maaServerBaseUrl = ConfigStore.Global.getOrDefault(com.uid2.core.Const.Config.MaaServerBaseUrlProp, "https://sharedeus.eus.attest.azure.net");
                 attestationService.with("azure-cc", new AzureCCCoreAttestationService(maaServerBaseUrl, ConfigStore.Global.get(Const.Config.CorePublicUrlProp)));
+                attestationService.with("azure-cc-aks", new AzureCCCoreAttestationAksService(maaServerBaseUrl, ConfigStore.Global.get(Const.Config.CorePublicUrlProp)));
 
                 attestationService.with("gcp-oidc", new GcpOidcCoreAttestationService(corePublicUrl));
 
